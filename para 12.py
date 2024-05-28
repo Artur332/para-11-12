28ad541dcec730cc11ee02256bb6e8bb1ab9d5d0
Artur332 <166921752+Artur332@users.noreply.github.com>
2024-05-28 11:30:06 +0300

    Update para 12.py

--- para 12.py
+++ para 12.py
@@ -1,3 +1,52 @@
+ЗАВДАННЯ 2
+
+class Piece:
+    def __init__(self, color, position):
+        self.color = color
+        self.position = position
+
+    def move(self):
+        pass
+
+class Pawn(Piece):
+    def move(self):
+        print("пішак рухається на 1 або 2 клітини і атакує по діагоналі")
+
+class Rook(Piece):
+    def move(self):
+        print("Тура рухається горизонтально або вертикально")
+
+class Knight(Piece):
+    def move(self):
+        print("Кінь рухається у формі «L»")
+
+class Bishop(Piece):
+    def move(self):
+        print("Слон рухається по діагоналі на будьяку кількість полів")
+
+class Queen(Piece):
+    def move(self):
+        print("королева рухається по горизонталі або по вертикалі")
+
+class King(Piece):
+    def move(self):
+        print("король рухається на 1 клітку в любому напрямку")
+
+pawn = Pawn("Білий", "A2")
+rook = Rook("Чорний", "H8")
+knight = Knight("Білий", "G1")
+bishop = Bishop("Чоринй", "C3")
+queen = Queen("Білий", "D4")
+king = King("Чорний", "E5")
+
+pieces = [pawn, rook, knight, bishop, queen, king]
+
+for piece in pieces:
+    print(f"{piece.color} {piece.__class__.__name__} at position {piece.position}:")
+    piece.move()
+
+#ЗАВДЯННЯ 3
+
 import sqlite3
 
 connect = sqlite3.connect('employees')
